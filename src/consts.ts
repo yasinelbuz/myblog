f8177170c0c25a0a32be32b33f7a9277838bd742
Yasin Elbüz <yasinelbuz@gmail.com>
2023-11-20 07:36:45 +0300

    feat:redesing and add tailwindcss

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '🚴🏼';
+export const SITE_TITLE = 'Yasin Elbüz';
 export const SITE_DESCRIPTION = `Yasin Elbüz'ün web sayfasıdır.`;
