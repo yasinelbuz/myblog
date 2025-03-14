ab6f2953771bbb963e76335e73eccd97a469cd4e
Yasin Elbüz <yasinelbuz@gmail.com>
2025-03-14 03:36:11 +0300

    chore: update astro.config.mjs to add Vercel adapter and set output to server; update package.json and package-lock.json to include @astrojs/vercel dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import { vercel } from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yasinelbuz.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+  output: 'server',
+  adapter: vercel()
 });
