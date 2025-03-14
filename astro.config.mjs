eccb0e93e96814b2f25cc8174bdd4b70129557c6
Yasin Elbüz <yasinelbuz@gmail.com>
2025-03-14 03:37:54 +0300

    chore: update import for Vercel adapter in astro.config.mjs to use serverless version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { vercel } from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
